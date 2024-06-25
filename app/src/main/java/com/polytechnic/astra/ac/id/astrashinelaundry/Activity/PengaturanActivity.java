649f28f0db626b6119d5f56cf81e2d4a11a14198
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-06-25 10:39:17 +0700

    Reset-pw dan juga index pengaturan done

---
+++ app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/Activity/PengaturanActivity.java
@@ -0,0 +1,16 @@
+package com.polytechnic.astra.ac.id.astrashinelaundry.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.polytechnic.astra.ac.id.astrashinelaundry.R;
+
+public class PengaturanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengaturan);
+    }
+}
---
+++ app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/Fragment/PengaturanFragment.java
@@ -0,0 +1,26 @@
+package com.polytechnic.astra.ac.id.astrashinelaundry.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.polytechnic.astra.ac.id.astrashinelaundry.R;
+
+public class PengaturanFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pengaturan, container, false);
+    }
+}
