aef3e2fdfef319ae3b3adcdc8ef2f69f4dab5a13
halizawidya <halizawidyas@gmail.com>
2024-07-10 08:25:33 +0700

    customer

--- app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/API/VO/DurasiVo.java
+++ app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/API/VO/DurasiVo.java
@@ -1,13 +1,13 @@
 package com.polytechnic.astra.ac.id.astrashinelaundry.API.VO;
 
-import com.polytechnic.astra.ac.id.astrashinelaundry.Model.DetailTransaksiModel;
+import com.polytechnic.astra.ac.id.astrashinelaundry.Model.DurasiModel;
 
 import java.util.List;
 
-public class DetailTransaksiVo {
+public class DurasiVo {
     private int status;
 
-    private List<DetailTransaksiModel> data;
+    private DurasiModel data;
 
     private String message;
 
@@ -19,11 +19,11 @@ public class DetailTransaksiVo {
         this.status = status;
     }
 
-    public List<DetailTransaksiModel> getData() {
+    public DurasiModel getData() {
         return data;
     }
 
-    public void setData(List<DetailTransaksiModel> data) {
+    public void setData(DurasiModel data) {
         this.data = data;
     }
 
---
+++ app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/Fragment/ViewTransaksiFragment.java
@@ -0,0 +1,301 @@
+package com.polytechnic.astra.ac.id.astrashinelaundry.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.polytechnic.astra.ac.id.astrashinelaundry.API.Repository.TransaksiRepository;
+import com.polytechnic.astra.ac.id.astrashinelaundry.API.VO.TransaksiListVO;
+import com.polytechnic.astra.ac.id.astrashinelaundry.Activity.MainActivity;
+import com.polytechnic.astra.ac.id.astrashinelaundry.Model.TransaksiModel;
+import com.polytechnic.astra.ac.id.astrashinelaundry.Model.UserModel;
+import com.polytechnic.astra.ac.id.astrashinelaundry.R;
+import com.polytechnic.astra.ac.id.astrashinelaundry.ViewModel.TransaksiListViewModel;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ViewTransaksiFragment extends Fragment {
+    private TransaksiListViewModel mTransaksiListViewModel;
+    private RecyclerView mTransaksiRecyclerView;
+    private TransaksiAdapter mAdapter;
+    private TabLayout mTabLayout;
+    private String status = "Pick Up";
+    private Button mButtonSetting;
+    private FloatingActionButton mButtonAdd;
+
+    public ViewTransaksiFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TransaksiRepository.initialize(requireContext());
+        mTransaksiListViewModel = new ViewModelProvider(this).get(TransaksiListViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_transaksi, container, false);
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("loginSession", Context.MODE_PRIVATE);
+        String userJson = sharedPreferences.getString("dataUser", null);
+
+        final UserModel user;
+        if (userJson != null) {
+            Gson gson = new Gson();
+            user = gson.fromJson(userJson, UserModel.class);
+        } else {
+            user = null;
+        }
+
+        // Mengambil TextView dari layout
+        TextView txtName = view.findViewById(R.id.txt_name);
+        if (user != null) {
+            txtName.setText("Halo, " + user.getNamaUser());
+        } else {
+            txtName.setText("Halo, Pengguna");
+        }
+
+        TextView txtDate = view.findViewById(R.id.txt_date);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMM yyyy", new Locale("id", "ID"));
+        String todayDate = dateFormat.format(calendar.getTime());
+        txtDate.setText(todayDate);
+
+        mTransaksiRecyclerView = view.findViewById(R.id.list_transaksi);
+        mTransaksiRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new TransaksiAdapter(requireContext(), user);
+        mTransaksiRecyclerView.setAdapter(mAdapter);
+
+        // Mengambil elemen lain yang ingin diatur visibilitasnya
+        ImageView imgDataEmpty = view.findViewById(R.id.img_data_empty);
+        TextView txtEmptyMessage = view.findViewById(R.id.txt_empty_message);
+        TextView txtEmptyMessage2 = view.findViewById(R.id.txt_empty_message2);
+        Button mAddButtonEmpty = view.findViewById(R.id.btn_tambah);
+
+        // Mengatur TabLayout
+        mTabLayout = view.findViewById(R.id.tabLayout);
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0: // Tab "PickUp"
+                        status = "Pick Up";
+                        break;
+                    case 1: // Tab "Proses"
+                        status = "Proses";
+                        break;
+                    case 2: // Tab "Selesai"
+                        status = "Selesai";
+                        break;
+                }
+                loadData(user, status, imgDataEmpty, txtEmptyMessage, txtEmptyMessage2, mAddButtonEmpty);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        loadData(user, status, imgDataEmpty, txtEmptyMessage, txtEmptyMessage2, mAddButtonEmpty);
+
+        mButtonSetting = view.findViewById(R.id.btn_setting);
+        mButtonSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PengaturanFragment fragmentPengaturan = new PengaturanFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                // Ganti fragment dan tambahkan ke back stack
+                transaction.replace(R.id.fragment_view_transaksi, fragmentPengaturan);
+                transaction.addToBackStack(null);  // Menambahkan ke back stack untuk bisa kembali
+                transaction.commit();
+            }
+        });
+
+        mButtonAdd = view.findViewById(R.id.btn_add);
+        mButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(v.getContext());
+                View bottomSheetView = getLayoutInflater().inflate(R.layout.fragment_add_transaksi, null);
+                bottomSheetDialog.setContentView(bottomSheetView);
+
+                Button closeButton = bottomSheetView.findViewById(R.id.btn_kembali);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                bottomSheetDialog.show();
+            }
+        });
+
+        mAddButtonEmpty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(v.getContext());
+                View bottomSheetView = getLayoutInflater().inflate(R.layout.fragment_add_transaksi, null);
+                bottomSheetDialog.setContentView(bottomSheetView);
+
+                Button closeButton = bottomSheetView.findViewById(R.id.btn_kembali);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                bottomSheetDialog.show();
+            }
+        });
+
+        return view;
+    }
+
+    private void loadData(UserModel user, String status, ImageView imgDataEmpty, TextView txtEmptyMessage, TextView txtEmptyMessage2, Button mAddButton) {
+        if (user != null) {
+            mAdapter.clear();
+            mTransaksiListViewModel.getTransaksiByIdAndStatus(user.getIdUser(), status);
+            mTransaksiListViewModel.getAllTransaksiResponse().observe(getViewLifecycleOwner(), new Observer<TransaksiListVO>() {
+                @Override
+                public void onChanged(TransaksiListVO transaksiListVO) {
+                    if (transaksiListVO != null && !transaksiListVO.getData().isEmpty()) {
+                        Log.e("ViewTransaksiFragment", "Data received: " + transaksiListVO.getData().size());
+                        mAdapter.setTransaksiList(transaksiListVO.getData());
+                        mTransaksiRecyclerView.setVisibility(View.VISIBLE);
+                        imgDataEmpty.setVisibility(View.GONE);
+                        txtEmptyMessage.setVisibility(View.GONE);
+                        txtEmptyMessage2.setVisibility(View.GONE);
+                        mAddButton.setVisibility(View.GONE);
+                    } else {
+                        Log.e("ViewTransaksiFragment", "TransaksiListVO is null or empty");
+                        mAdapter.setTransaksiList(new ArrayList<>()); // Clear the list if null or empty
+                        mTransaksiRecyclerView.setVisibility(View.GONE);
+                        imgDataEmpty.setVisibility(View.VISIBLE);
+                        txtEmptyMessage.setVisibility(View.VISIBLE);
+                        txtEmptyMessage2.setVisibility(View.VISIBLE);
+                        mAddButton.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
+    }
+
+    public class TransaksiAdapter extends RecyclerView.Adapter<ViewTransaksiFragment.TransaksiAdapter.TransaksiViewHolder> {
+        private List<TransaksiModel> transaksiList = new ArrayList<>();
+        private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        private UserModel user;
+        private Context context;
+
+        public TransaksiAdapter(Context context, UserModel user) {
+            this.context = context;
+            this.user = user;
+        }
+
+        @NonNull
+        @Override
+        public TransaksiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_listpickup_kurir, parent, false);
+            return new TransaksiViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TransaksiViewHolder holder, int position) {
+            TransaksiModel transaksi = transaksiList.get(position);
+            if (user != null) {
+                holder.bind(transaksi, user);
+                holder.itemView.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        // Open the detail fragment with the selected transaction
+                        CustomerRincianFragment customerRincianFragment = new CustomerRincianFragment();
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("transaksi", transaksi);
+                        customerRincianFragment.setArguments(bundle);
+
+                        getParentFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_view_transaksi, customerRincianFragment)  // Make sure R.id.PickUpKurir is correct
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                });
+            } else {
+                Log.e("ViewTransaksiFragment", "User is null");
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return transaksiList.size();
+        }
+
+        public void setTransaksiList(List<TransaksiModel> transaksiList) {
+            this.transaksiList.clear(); // Clear existing list
+            this.transaksiList.addAll(transaksiList); // Add new data
+            notifyDataSetChanged();
+        }
+
+        public void clear() {
+            this.transaksiList.clear();
+            notifyDataSetChanged();
+        }
+
+        class TransaksiViewHolder extends RecyclerView.ViewHolder {
+            private TextView mCustomer;
+            private TextView mTanggalPesanan;
+            private TextView mTanggalSelesai;
+
+            public TransaksiViewHolder(@NonNull View itemView) {
+                super(itemView);
+                mCustomer = itemView.findViewById(R.id.Customer);
+                mTanggalPesanan = itemView.findViewById(R.id.tanggal_pesanan);
+                mTanggalSelesai = itemView.findViewById(R.id.tanggal_selesai);
+            }
+
+            public void bind(TransaksiModel transaksi, UserModel userModel) {
+                Log.e("BIND", "Binding data for user: " + userModel.getIdUser());
+                mCustomer.setText("NOTA-00" + transaksi.getIdTransaksi());
+                mTanggalPesanan.setText("Tanggal Pesanan: " + dateFormat.format(transaksi.getTanggalPesanan()));
+                mTanggalSelesai.setText("Estimasi Selesai: " + dateFormat.format(transaksi.getTanggalPengiriman()));
+            }
+        }
+    }
+}
