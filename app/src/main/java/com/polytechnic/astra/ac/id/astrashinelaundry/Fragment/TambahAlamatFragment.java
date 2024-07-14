a9711816bb525ce529dddac8f42ae683d563eb40
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-07-14 08:57:19 +0700

    Maps done

--- app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/Fragment/TambahAlamatFragment.java
+++ app/src/main/java/com/polytechnic/astra/ac/id/astrashinelaundry/Fragment/TambahAlamatFragment.java
@@ -2,65 +2,51 @@ package com.polytechnic.astra.ac.id.astrashinelaundry.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.polytechnic.astra.ac.id.astrashinelaundry.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TambahAlamatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TambahAlamatFragment extends Fragment {
+public class TambahAlamatFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MapView mMapView;
+    private GoogleMap mGoogleMap;
 
     public TambahAlamatFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TambahAlamatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TambahAlamatFragment newInstance(String param1, String param2) {
-        TambahAlamatFragment fragment = new TambahAlamatFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tambah_alamat, container, false);
+        View view = inflater.inflate(R.layout.fragment_tambah_alamat, container, false);
+        mMapView = view.findViewById(R.id.maps_view);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.mGoogleMap = googleMap;
+        LatLng location = new LatLng(55.6761, 12.5685);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Companagen"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12));
     }
 }
